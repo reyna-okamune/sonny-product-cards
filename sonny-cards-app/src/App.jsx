807636cc9a9f4e0d64bb5306f149cba368f3ca0c
rokamune <reynaoaguirre@gmail.com>
2025-04-30 10:06:39 -0700

    UI for Add to Cart Button

--- sonny-cards-app/src/App.jsx
+++ sonny-cards-app/src/App.jsx
@@ -4,13 +4,13 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-col lg:flex-row w-full">
+      <div className="flex flex-col md:flex-row w-full">
         <div className='flex-col items-center'>
           <h2 className="font-poppins text-3xl text-stone-800 font-bold pb-5 text-center sm:text-left">Sonny Angels</h2>
           < ProductList />
         </div>
 
-        <div className=" border-1 border-green-500">
+        <div className="">
           <p>shopping cart</p>
         </div>
       </div>
--- sonny-cards-app/src/ProductCard.jsx
+++ sonny-cards-app/src/ProductCard.jsx
@@ -2,25 +2,34 @@
 export default function ProductCard(props) {
     return (
         <>
-            <div className="flex flex-col items-center content-center p-2 w-full max-w-64 ">
-                
+            <div className="flex flex-col items-center content-center p-2 mb-10 w-full max-w-64 ">
+
                 <img 
                     src={props.image} 
                     alt="Sonny Icon" 
                     className="bg-white w-full h-auto object-cover mb-4 rounded-md"
                 />
-                
+
                 {/* details of product */}
                 <div className="text-left w-full">
                     <p className="text-stone-500 text-xs">
                         {props.category}
                     </p>
-                    <h3 className="text-stone-800 lg:text-xl md:text-lg font-bold">
+                    <h3 className="text-stone-800 sm:text-lg md:text-xl font-bold">
                         {props.name}
                     </h3>
                     <p className="text-rose-600 text-xs font-semibold">
                         ${props.price}
                     </p>
+                </div>
+
+                <div className="pt-3">
+                    <button className="w-full flex text-sm font-semibold gap-2 bg-white py-3 px-8 rounded-full border-2 border-rose-300 hover:border-rose-500">
+                        <span className="material-symbols-outlined text-sm">
+                            add_shopping_cart
+                        </span>
+                        Add to Cart
+                    </button>
                 </div>
 
 
